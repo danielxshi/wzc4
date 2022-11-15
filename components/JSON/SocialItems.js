473b55f360eb6a17e67656e7f91aa9a2a6a1a402
Daniel <danielshi96@gmail.com>
2022-11-14 17:47:02 -0800

    updating social item

--- components/JSON/SocialItems.js
+++ components/JSON/SocialItems.js
@@ -1,6 +1,6 @@
 import { ImLinkedin2 } from 'react-icons/im';
 import { FaFacebookF } from 'react-icons/fa';
-import WeChat from "../../public/images/logos/nav-logo.png"
+import WeChat from "../../public/images/logos/wechat.png"
 
 const WeChatLogo = WeChat;
 
