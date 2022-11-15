0e3dd36258880b113df1363a2c54c7f5e947e631
Daniel <danielshi96@gmail.com>
2022-11-14 17:42:39 -0800

    Adjusting social items for case sensitivity

--- components/JSON/SocialItems.js
+++ components/JSON/SocialItems.js
@@ -1,11 +1,11 @@
-import { ImLinkedin2 } from 'react-icons/im';
-import { FaFacebookF } from 'react-icons/fa';
-import WeChat from "../../public/images/logos/nav-logo.png"
+// import { ImLinkedin2 } from 'react-icons/im';
+// import { FaFacebookF } from 'react-icons/fa';
+// import WeChat from "../../public/images/logos/nav-logo.png"
 
-const WeChatLogo = WeChat;
+// const WeChatLogo = WeChat;
 
-const SocialItems = {
-  WeChat: WeChat
-};
+// const SocialItems = {
+//   WeChat: WeChat
+// };
 
-export default SocialItems;
+// export default SocialItems;
