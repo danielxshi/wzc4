bb928320f0b75b11fac04bc9e166bcf3b518fe56
Daniel <danielshi96@gmail.com>
2022-12-11 02:12:23 -0800

    Updating mobile nav

--- components/navigation/MobileNav.js
+++ components/navigation/MobileNav.js
@@ -4,62 +4,68 @@ import { useRouter } from "next/router";
 import React, { useState } from "react";
 import MenuItems from "../JSON/MenuItems";
 
-export const renderSwitch = (params) => {
-  const router = useRouter();
-  if (!params) {
-    return "";
-  }
-  const keys = Object.keys(params);
-  const value = params[keys[0]];
-  const test = params[keys[1]];
+// export const renderSwitch = (params) => {
+//   const router = useRouter();
+//   if (!params) {
+//     return "";
+//   }
+//   const keys = Object.keys(params);
+//   const value = params[keys[0]];
+//   const test = params[keys[1]];
 
-  switch (keys[0]) {
-    case "menu":
-      return (
-        <div className="menu--container">
-          {value.map((item, index) => (
-            <div key={index} className="dropdown">
-              <Link href={item.url}>
-                <a
-                  // key={index}
-                  className={
-                    router.pathname == `${item.url}` ||
-                    router.pathname == `${item.p1}` ||
-                    router.pathname == `${item.p2}` ||
-                    router.pathname == `${item.p3}` ||
-                    router.pathname == `${item.p4}`
-                      ? "active"
-                      : ""
-                  }
-                >
-                  {item.title}
-                </a>
-              </Link>
-              <ul className="dropdown-content">
-                {item.submenu.map((item2, index2) => {
-                  return (
-                    <Link className="whitespace-nowrap" href={item2.url}>
-                      <a
-                        key={index2}
-                        className={
-                          router.pathname == `${item2.url}` ? "superactive" : ""
-                        }
-                      >
-                        {item2.subtitle}
-                      </a>
-                    </Link>
-                  );
-                })}
-              </ul>
-            </div>
-          ))}
-        </div>
-      );
+//   switch (keys[0]) {
+//     case "menu":
+//       return (
+//         <div className={style["mobile--menu--container"]}>
+//           {value.map((item, index) => (
+//             <div key={index} className={style[""]}>
+//               <h4 className={style["link--header"]}>
+//                 <Link href={item.url}>
+//                   <a
+//                     // key={index}
+//                     className={
+//                       router.pathname == `${item.url}` ||
+//                       router.pathname == `${item.p1}` ||
+//                       router.pathname == `${item.p2}` ||
+//                       router.pathname == `${item.p3}` ||
+//                       router.pathname == `${item.p4}`
+//                         ? "active"
+//                         : ""
+//                     }
+//                   >
+//                     {item.title}
+//                   </a>
+//                 </Link>
+//               </h4>
+//               <ul className="">
+//                 {item.submenu.map((item2, index2) => {
+//                   return (
+//                     <li>
+//                       <Link className="whitespace-nowrap" href={item2.url}>
+//                         <a
+//                           key={index2}
+//                           className={
+//                             router.pathname == `${item2.url}`
+//                               ? "superactive"
+//                               : ""
+//                           }
+//                         >
+//                           {item2.subtitle}
+//                         </a>
+//                       </Link>
+//                     </li>
+//                   );
+//                 })}
+//               </ul>
+//             </div>
+//           ))}
+//         </div>
+//       );
 
-    default:
-      return "";
-  }
-};
+//     default:
+//       return "";
+//   }
+// };
 
 export default function MobileMenu() {
   const [click, setClick] = useState(false);
@@ -99,45 +105,64 @@ export default function MobileMenu() {
         }
       >
         <ul className={style["mobile-nav-menu"]}>
-          {MenuItems.NavMenuItems.map((item, index) => {
+          {MenuItems.NavMenuItems2.map((items, index) => {
             return (
-              <li onClick={closeMobileMenu} key={index}>
-                <div
-                  className={
-                    router.pathname == item.pName
-                      ? style["active-link"]
-                      : style["mobile-nav--wrapper"]
-                  }
-                >
-                  <Link
-                    href={item.url}
-                    passHref={true}
-                    smooth={true}
-                    spy={true}
-                  >
-                    <a
-                      key={index}
-                      className={
-                        router.pathname == `${item.url}` ? "mobile--active" : ""
-                      }
-                    >
-                      {item.title}
-                    </a>
-                  </Link>
-                </div>
-              </li>
-            );
-          })}
-          {/* {MenuItems.NavMenuItems2.map((items, index) => {
-            return (
-              <div
+              <li
+                onClick={handleClick}
                 className={[style["menu--content--item"]].join(" ")}
                 key={index}
               >
-                {renderSwitch(items)}
-              </div>
+                <div className={style["mobile--menu--container"]}>
+                  {items.menu.map((item, index) => (
+                    <div key={index} className={style[""]}>
+                      <h4 className={style["link--header"]}>
+                        <Link href={item.url}>
+                          <a
+                            // key={index}
+                            className={
+                              router.pathname == `${item.url}` ||
+                              router.pathname == `${item.p1}` ||
+                              router.pathname == `${item.p2}` ||
+                              router.pathname == `${item.p3}` ||
+                              router.pathname == `${item.p4}`
+                                ? "active"
+                                : ""
+                            }
+                          >
+                            {item.title}
+                          </a>
+                        </Link>
+                      </h4>
+                      <ul className="">
+                        {item.submenu.map((item2, index2) => {
+                          return (
+                            <li>
+                              <Link
+                                className="whitespace-nowrap"
+                                href={item2.url}
+                              >
+                                <a
+                                  key={index2}
+                                  className={
+                                    router.pathname == `${item2.url}`
+                                      ? "superactive"
+                                      : ""
+                                  }
+                                >
+                                  {item2.subtitle}
+                                </a>
+                              </Link>
+                            </li>
+                          );
+                        })}
+                      </ul>
+                    </div>
+                  ))}
+                </div>
+                {/* {renderSwitch(items)} */}
+              </li>
             );
-          })} */}
+          })}
         </ul>
       </div>
     </nav>
