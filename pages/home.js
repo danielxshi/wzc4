b279eaa62480b6c80d16d4bab7f17a2aa20c56d0
Daniel <danielshi96@gmail.com>
2023-01-26 20:33:19 -0800

    Updating image

--- pages/home.js
+++ pages/home.js
@@ -1,21 +1,16 @@
 import NewsPreview from "../components/home/NewsPreview";
 import SelfIntro from "../components/home/About";
-import Carousel from "../components/header/Carousel";
 import ArticleSection from "../components/article/ArticleSection";
-import DeviceDetector from "../components/DeviceDetector";
 import React from "react";
 import President from "../components/home/President";
 
 export default function Home() {
   return (
     <>
-      {/* <DeviceDetector> */}
-      {/* <Carousel /> */}
       <President />
       <ArticleSection />
       <NewsPreview />
       <SelfIntro />
-      {/* </DeviceDetector> */}
     </>
   );
 }
